ffc22ba6ef44d5f9aa6aee65c8213e67b9702ff1
HPHAN1 <huyphan@rocketmortgage.com>
2022-01-18 00:08:05 -0500

    initial commit

---
+++ src/app/MenuItem.ts
@@ -0,0 +1,12 @@
+export class MenuItem{
+  Item:string;
+  Category:string;
+  Price:number;
+
+  constructor(Item:string, Category:string, Price:number){
+    this.Item = Item;
+    this.Category = Category;
+    this.Price = Price;
+  }
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from './MenuItem';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Menu';
+export class AppComponent implements OnInit{
+  items: MenuItem[] = [];
+  constructor(){}
+  ngOnInit(){
+  this.items = [
+    {Item:"Pizza", Category:"dinner", Price: 18.99},
+    {Item:"Chicken Wings", Category:"dinner", Price: 10.99},
+    {Item:"Chicken Fingers", Category:"sides", Price: 9.99},
+    {Item:"Cheesey Breadsticks", Category:"sides", Price: 7.99},
+    {Item:"Caesar Salad", Category:"salad", Price: 8.99},
+    {Item:"House Salad", Category:"salad", Price: 8.99},
+    {Item:"Cinnamon Roll", Category:"dessert", Price: 5.99},
+    {Item:"Chocolate Chip Cookie", Category:"dessert", Price: 5.99},
+  ];
+  }
 }
